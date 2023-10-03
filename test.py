53ba106c0062357b326b20d5cf4a7a797140e198
Mohammed Amir <muji-_000@MacBook-Air.local>
2023-10-03 07:53:59 -0400

    attempted dfs, stuck in infinite loop

--- test.py
+++ test.py
@@ -3,10 +3,6 @@ from solve_starter import *
 if __name__ == "__main__":
     board = read_from_file('test.txt')
     board.display()
-    state = State(board, 1, 0, 0, parent = None)
-    successors = get_successors(state)
-    print(len(successors))
-
-    for state in successors:
-        state.board.display()
     
+    path, cost = dfs(board)
+    print(cost)
