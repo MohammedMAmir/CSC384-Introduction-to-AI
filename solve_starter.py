4067a273931463e1bbbf27f81c9b1812964c683d
Mohammed Amir <muji-_000@MacBook-Air.local>
2023-10-03 07:51:51 -0400

    attempted dfs, stuck in infinite loop

--- solve_starter.py
+++ solve_starter.py
@@ -30,7 +30,6 @@ def is_goal(state):
     available_spots = state.board.storage.copy()
     for box in state.board.boxes:
         for spot in available_spots:
-            print(spot)
             if spot[0] == box[0] and spot[1] == box[1]:
                 available_spots.remove(spot)
                 break
@@ -53,8 +52,14 @@ def get_path(state):
     :return: The path.
     :rtype: List[State]
     """
+    path = []
+    path.insert(0,state)
 
-    raise NotImplementedError
+    currState = state
+    while(currState.depth > 0):
+        currState = currState.parent
+        path.insert(0,currState)
+    return path
 
 def is_obstacle(location, state):
     for obstacle in state.board.obstacles:
@@ -77,6 +82,15 @@ def is_robot(location, state):
 
     return False
 
+def copy_board(board: Board):
+    new_board = Board(board.name, board.width, board.height, copy.deepcopy(board.robots), 
+                      copy.deepcopy(board.boxes), copy.deepcopy(board.storage), copy.deepcopy(board.obstacles))
+    return new_board
+
+def copy_state(state: State):
+    new_state = State(copy_board(state.board), state.hfn, state.f, state.depth, parent=None)
+    return new_state
+
 def get_successors(state):
     """
     Return a list containing the successor states of the given state.
@@ -87,7 +101,7 @@ def get_successors(state):
     :return: The list of successor states.
     :rtype: List[State]
     """
-    temp_state = copy.deepcopy(state)
+    temp_state = copy_state(state)
     state_list = []
     temp_state.depth = state.depth + 1
     temp_state.parent = state
@@ -109,20 +123,19 @@ def get_successors(state):
                             listRobot = list(temp_state.board.robots[robIndex])
                             listRobot[1] += 1
                             temp_state.board.robots[robIndex] = tuple(listRobot)
-                            state_list.append(copy.deepcopy(temp_state))
+                            state_list.append(copy_state(temp_state))
 
                             #Reset temp state
                             listRobot[1] -= 1
                             listBox[1] -= 1
                             temp_state.board.boxes[boxIndex] = tuple(listBox)
                             temp_state.board.robots[robIndex] = tuple(listRobot)
-                        break
             #The case where there is no box or obstacle obstructing the robots path            
             else:
                 listRobot = list(temp_state.board.robots[robIndex])
                 listRobot[1] += 1
                 temp_state.board.robots[robIndex] = tuple(listRobot)
-                state_list.append(copy.deepcopy(temp_state))
+                state_list.append(copy_state(temp_state))
 
                 #Reset temp state
                 listRobot[1] -= 1
@@ -144,24 +157,25 @@ def get_successors(state):
                             listRobot = list(temp_state.board.robots[robIndex])
                             listRobot[1] -= 1
                             temp_state.board.robots[robIndex] = tuple(listRobot)
-                            state_list.append(copy.deepcopy(temp_state))
+                            state_list.append(copy_state(temp_state))
 
                             #Reset temp state
                             listRobot[1] += 1
                             listBox[1] += 1
                             temp_state.board.boxes[boxIndex] = tuple(listBox)
                             temp_state.board.robots[robIndex] = tuple(listRobot)
-                        break
+                    
             #The case where there is no box or obstacle obstructing the robots path            
             else:
                 listRobot = list(temp_state.board.robots[robIndex])
                 listRobot[1] -= 1
                 temp_state.board.robots[robIndex] = tuple(listRobot)
-                state_list.append(copy.deepcopy(temp_state))
+                state_list.append(copy_state(temp_state))
 
                 #Reset temp state
                 listRobot[1] += 1
                 temp_state.board.robots[robIndex] = tuple(listRobot)
+    
 
         #see if robot can move left:
         if is_obstacle((robot[0]-1, robot[1]), temp_state) == False and is_robot((robot[0]-1, robot[1]), temp_state) == False:
@@ -179,24 +193,25 @@ def get_successors(state):
                             listRobot = list(temp_state.board.robots[robIndex])
                             listRobot[0] -= 1
                             temp_state.board.robots[robIndex] = tuple(listRobot)
-                            state_list.append(copy.deepcopy(temp_state))
+                            state_list.append(copy_state(temp_state))
 
                             #Reset temp state
                             listRobot[0] += 1
                             listBox[0] += 1
                             temp_state.board.boxes[boxIndex] = tuple(listBox)
                             temp_state.board.robots[robIndex] = tuple(listRobot)
-                        break
+ 
             #The case where there is no box or obstacle obstructing the robots path            
             else:
                 listRobot = list(temp_state.board.robots[robIndex])
                 listRobot[0] -= 1
                 temp_state.board.robots[robIndex] = tuple(listRobot)
-                state_list.append(copy.deepcopy(temp_state))
+                state_list.append(copy_state(temp_state))
 
                 #Reset temp state
                 listRobot[0] += 1
                 temp_state.board.robots[robIndex] = tuple(listRobot)
+
 
         #see if robot can move right:
         if is_obstacle((robot[0]+1, robot[1]), temp_state) == False and is_robot((robot[0]+1, robot[1]), temp_state) == False:
@@ -205,7 +220,7 @@ def get_successors(state):
                 if(is_obstacle((robot[0]+2, robot[1]), temp_state) == False and 
                 is_box((robot[0]+2, robot[1]), temp_state) == False and
                 is_robot((robot[0]+2, robot[1]), temp_state) == False):
-                    for box in temp_state.board.boxes:
+                    for boxIndex in range(len(temp_state.board.boxes)):
                         if (temp_state.board.boxes[boxIndex][0] == robot[0]+1 and 
                         temp_state.board.boxes[boxIndex][1] == robot[1]):
                             listBox = list(temp_state.board.boxes[boxIndex])
@@ -214,28 +229,38 @@ def get_successors(state):
                             listRobot = list(temp_state.board.robots[robIndex])
                             listRobot[0] += 1
                             temp_state.board.robots[robIndex] = tuple(listRobot)
-                            state_list.append(copy.deepcopy(temp_state))
+                            state_list.append(copy_state(temp_state))
 
                             #Reset temp state
                             listRobot[0] -= 1
                             listBox[0] -= 1
                             temp_state.board.boxes[boxIndex] = tuple(listBox)
                             temp_state.board.robots[robIndex] = tuple(listRobot)
-                        break
             #The case where there is no box or obstacle obstructing the robots path            
             else:
                 listRobot = list(temp_state.board.robots[robIndex])
                 listRobot[0] += 1
                 temp_state.board.robots[robIndex] = tuple(listRobot)
-                state_list.append(copy.deepcopy(temp_state))
+                state_list.append(copy_state(temp_state))
 
                 #Reset temp state
                 listRobot[0] -= 1
                 temp_state.board.robots[robIndex] = tuple(listRobot)
+
     
 
     return state_list
     raise NotImplementedError
+
+def multi_prune(state, visited):
+    for i in visited:
+        if i == state.id:
+            return True
+
+    return False
+
+def moveState(direction, pos_Neg, temp_State, robIndex):
+    return
 
 def dfs(init_board):
     """
@@ -252,6 +277,31 @@ def dfs(init_board):
     :rtype: List[State], int
     """
 
+    intialState = State(init_board, 0, 0, 0, None)
+    frontier = []
+    visited = []
+    visited.append(intialState.id)
+    frontier.append(intialState)
+    count = 0
+    while len(frontier) > 0 and count < 100000:
+        currState = frontier.pop()
+        ##currState.board.display()
+        if is_goal(currState):
+            return get_path(currState), len(get_path(currState))
+        else:
+            successors_temp = get_successors(currState)
+            successors_prune = []
+            for succ_index in range(len(successors_temp)):
+                if multi_prune(successors_temp[succ_index], visited) == False:
+                    successors_prune.append(successors_temp[succ_index])
+                    visited.append(successors_temp[succ_index].id)
+            if len(successors_prune) != 0:
+                frontier.extend(successors_prune)
+        count+=1
+        if count == 9999:
+            currState.board.display()
+
+    return [], -1
     raise NotImplementedError
 
 
